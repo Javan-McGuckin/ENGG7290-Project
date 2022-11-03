d400c031c6a630da180ef8a693e373705245a80b
Javan-McGuckin <116236156+Javan-McGuckin@users.noreply.github.com>
2022-11-03 12:40:36 +1000

    Modified to include plots of COD pCOD sCOD TSS and VSS against the influent and efflent data after accounting for the correction for SRT

--- plotting_charts.m
+++ plotting_charts.m
@@ -1,175 +1,187 @@
-% USER: Javan McGuckin - October 2022
+% % USER: Javan McGuckin - October 2022
+% 
+% % DESCRIPTION: This code takes the data from the digestor out matrix from
+% % the simulink simulation and plots it as a time series for visualisation
+% 
+% close all;
+% %% VARIABLE DEFINITION
+% load PILOT_DATA_ALL.mat
+% 
+% pH  = digesterout(:,28);
+% 
+% Total_gas_flow              = digesterout(:,54);
+% Methane_flow_volume         = (digesterout(:,43)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54)*1.4;
+% Hydrogen_flow_Volume        = (digesterout(:,42)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54)*11.126;
+% CarbonDioxide_flow_Volume   = (digesterout(:,44)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54)*1.836;
+% 
+% Methane_flow_mass         = (digesterout(:,43)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54);
+% Hydrogen_flow_mass        = (digesterout(:,42)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54);
+% CarbonDioxide_flow_mass   = (digesterout(:,44)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54);
+% 
+% Acetate     = digesterout(:,7);
+% Propionate  = digesterout(:,6);
+% Butyrate    = digesterout(:,5);
+% Valerate    = digesterout(:,4);
+% %% PLOTTING GAS FLOWS
+% % Plot of the composition and volume of gas flow over the whole time period
+% % of the data
+% figure
+% subplot(2,3,1:2)
+% 
+% area(time, [Methane_flow_volume Hydrogen_flow_Volume CarbonDioxide_flow_Volume])
+% 
+% title("Modelled Gas flow composition chart by Volume")
+% grid on
+% xlabel("Time (Days)")
+% ylabel("Total Gas Flow (m^3/Day)")
+% legend("Methane","Hydrogen","Carbon Dioxide")
+% axis([0 600 0 0.125])
+% 
+% % Plot of the total gas flow against the measured gas flow for a HRT of 8
+% % days
+% subplot(2,3,4:6)
+% hold on
+% scatter(PILOT_DATA_ALL(:,1)+127,PILOT_DATA_ALL(:,4))
+% plot(time,Total_gas_flow,'k')
+% grid on
+% axis([127 566 0 0.1])
+% 
+% title("Modelled Gas FLow vs Gas Flow Data")
+% xlabel("Time (Days)")
+% ylabel("Total Gas Flow (m^3/Day)")
+% legend("Data","Modelled Preformance")
+% 
+% % Plotting pH
+% subplot(2,3,3)
+% 
+% plot(time,pH)
+% 
+% xline(127)
+% xline(277)
+% xline(401)
+% %axis([0 600 0 8])
+% title("pH over time")
+% grid on
+% xlabel("Time (Days)")
+% ylabel("pH")
+% legend("pH")
 
-% DESCRIPTION: This code takes the data from the digestor out matrix from
-% the simulink simulation and plots it as a time series for visualisation
+%% TSS VSS MSS
+load COD_SS_DATA.mat
+clf
+% COD
+subplot(5,2,1)
 
-close all;
-%% VARIABLE DEFINITION
-load PILOT_DATA_ALL.mat
+scatter(COD_SS_DATA(:,1),COD_SS_DATA(:,2))
 
-pH  = digesterout(:,28);
-
-Total_gas_flow              = digesterout(:,54);
-Methane_flow_volume         = (digesterout(:,43)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54)*1.4;
-Hydrogen_flow_Volume        = (digesterout(:,42)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54)*11.126;
-CarbonDioxide_flow_Volume   = (digesterout(:,44)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54)*1.836;
-
-Methane_flow_mass         = (digesterout(:,43)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54);
-Hydrogen_flow_mass        = (digesterout(:,42)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54);
-CarbonDioxide_flow_mass   = (digesterout(:,44)./digesterout(:,53)*P_atm*16/(R_cte*T_op)).*digesterout(:,54);
-
-Acetate     = digesterout(:,7);
-Propionate  = digesterout(:,6);
-Butyrate    = digesterout(:,5);
-Valerate    = digesterout(:,4);
-%% PLOTTING GAS FLOWS
-% Plot of the composition and volume of gas flow over the whole time period
-% of the data
-figure
-subplot(2,3,1:2)
-
-area(time, [Methane_flow_volume Hydrogen_flow_Volume CarbonDioxide_flow_Volume])
-
-title("Modelled Gas flow composition chart by Volume")
-grid on
+title("COD","COD in influent (Modelled")
 xlabel("Time (Days)")
-ylabel("Total Gas Flow (m^3/Day)")
-legend("Methane","Hydrogen","Carbon Dioxide")
-axis([0 600 0 0.125])
+ylabel("COD (kg m^-^3)")
+grid on
+axis([127 566 0 2])
 
-% Plot of the total gas flow against the measured gas flow for a HRT of 8
-% days
-subplot(2,3,4)
+subplot(5,2,2)
 hold on
-scatter(PILOT_DATA_ALL(:,1)+127,PILOT_DATA_ALL(:,4))
-plot(time,Total_gas_flow,'k')
-grid on
-axis([127 277 0 0.1])
+scatter(COD_SS_DATA(:,1),COD_SS_DATA(:,7))
+plot(time,tCOD_out)
 
-title("Modelled Gas FLow vs Gas Flow Data","8 Day HRT")
+title("COD","COD in effluent (Modelled and Data)")
 xlabel("Time (Days)")
-ylabel("Total Gas Flow (m^3/Day)")
-legend("Data","Modelled Preformance")
+ylabel("COD (kg m^-^3)")
+grid on
+axis([127 566 0 2])
+legend("Data","Model")
 
-% Plot of the total gas flow against the measured gas flow for a HRT of 4
-% days
-subplot(2,3,5)
+% sCOD
+subplot(5,2,3)
+
+scatter(COD_SS_DATA(:,1),COD_SS_DATA(:,3))
+
+title("sCOD","sCOD in influent (Modelled")
+xlabel("Time (Days)")
+ylabel("sCOD (kg m^-^3)")
+grid on
+axis([127 566 0 1.5])
+
+subplot(5,2,4)
 hold on
-scatter(PILOT_DATA_ALL(:,1)+127,PILOT_DATA_ALL(:,4))
-plot(time,Total_gas_flow,'k')
+scatter(COD_SS_DATA(:,1),COD_SS_DATA(:,8))
+plot(time,sCOD_out)
 
-grid on
-axis([277 401 0 0.1])
-
-title("Modelled Gas FLow vs Gas Flow Data","4 Day HRT")
+title("sCOD","sCOD in effluent (Modelled and Data)")
 xlabel("Time (Days)")
-ylabel("Total Gas Flow (m^3/Day)")
-legend("Data","Modelled Preformance")
+ylabel("sCOD (kg m^-^3)")
+grid on
+axis([127 566 0 1.5])
+legend("Data","Model")
 
-% Plot of the total gas flow against the measured gas flow for a HRT of 2
-% days
-subplot(2,3,6)
+% pCOD
+subplot(5,2,5)
+
+scatter(COD_SS_DATA(:,1),COD_SS_DATA(:,4))
+
+title("pCOD","pCOD in influent (Modelled")
+xlabel("Time (Days)")
+ylabel("pCOD (kg m^-^3)")
+grid on
+axis([127 566 0 1.5])
+
+subplot(5,2,6)
 hold on
-scatter(PILOT_DATA_ALL(:,1)+127,PILOT_DATA_ALL(:,4))
-plot(time,Total_gas_flow,'k')
+scatter(COD_SS_DATA(:,1),COD_SS_DATA(:,9))
+plot(time,pCOD_out)
 
-grid on
-axis([401 566 0 0.1])
-
-title("Modelled Gas FLow vs Gas Flow Data","2 Day HRT")
+title("pCOD","pCOD in effluent (Modelled and Data)")
 xlabel("Time (Days)")
-ylabel("Total Gas Flow (m^3/Day)")
-legend("Data","Modelled Preformance")
-
-%% Plotting pH
-subplot(2,3,3)
-
-plot(time,pH)
-
-xline(127)
-xline(277)
-xline(401)
-%axis([0 600 0 8])
-title("pH over time")
+ylabel("pCOD (kg m^-^3)")
 grid on
+axis([127 566 0 1.5])
+legend("Data","Model")
+
+% TSS
+subplot(5,2,7)
+
+scatter(COD_SS_DATA(:,1),COD_SS_DATA(:,5))
+
+title("TSS","TSS in influent (Modelled")
 xlabel("Time (Days)")
-ylabel("pH")
-legend("pH")
-
-%% Solids
-figure
-
-% CARBOHYDRATES IN
-subplot(3,2,1)
-plot(time,digesterin(:,14),'r')
-title("Carbohydrate concentration in","From Dynamic Input Data")
-
+ylabel("TSS (kg m^-^3)")
 grid on
-xlabel("Time (Days)")
-ylabel("Concentration (COD / m^3)")
-axis([127 566 0 0.25])
+axis([127 566 0 1])
 
-% CARBOHYDRATES OUT
-subplot(3,2,2)
-plot(time,digesterout(:,14),'r')
-title("Carbohydrate concentration out","From Model")
-
-grid on
-xlabel("Time (Days)")
-ylabel("Concentration (COD / m^3)")
-axis([127 566 0 0.25])
-
-% PROTEINES IN
-subplot(3,2,3)
-plot(time,digesterin(:,15),'g')
-title("Proteins concentration in","From Dynamic Input Data")
-grid on
-xlabel("Time (Days)")
-ylabel("Concentration (COD / m^3)")
-axis([127 566 0 0.25])
-
-% PROTEINS OUT
-subplot(3,2,4)
-plot(time,digesterout(:,15),'g')
-title("Proteins concentration out","From Model")
-
-grid on
-xlabel("Time (Days)")
-ylabel("Concentration (COD / m^3)")
-axis([127 566 0 0.25])
-
-% LIPIDS IN
-subplot(3,2,5)
-plot(time,digesterin(:,16),'b')
-title("Lipids concentration in","From Dynamic Input Data")
-
-grid on
-xlabel("Time (Days)")
-ylabel("Concentration (COD / m^3)")
-axis([127 566 0 0.125])
-
-% LIPIDS OUT
-subplot(3,2,6)
-plot(time,digesterout(:,16),'b')
-title("Lipids concentration out","From Model")
-
-grid on
-xlabel("Time (Days)")
-ylabel("Concentration (COD / m^3)")
-axis([127 566 0 0.125])
-
-%% Mineral Solids
-figure
-
+subplot(5,2,8)
 hold on
-plot(time,digesterin(:,27),'r')
-plot(time,digesterout(:,33),'b')
-title("Mineral Solids","Concentrations in and out of model")
+scatter(COD_SS_DATA(:,1),COD_SS_DATA(:,10))
+plot(time,TSS_out)
 
+title("TSS","TSS in effluent (Modelled and Data)")
+xlabel("Time (Days)")
+ylabel("TSS (kg m^-^3)")
 grid on
-xlabel("Time (days)")
-ylabel("Concentration (kg COD / m^3)")
-legend("Mineral Solids In","Mineral Solids Out")
+axis([127 566 0 1])
+legend("Data","Model")
 
+% VSS
+subplot(5,2,9)
+
+scatter(COD_SS_DATA(:,1),COD_SS_DATA(:,6))
+
+title("VSS","VSS in influent (Modelled")
+xlabel("Time (Days)")
+ylabel("VSS (kg m^-^3)")
+grid on
+axis([127 566 0 1])
+
+subplot(5,2,10)
+hold on
+scatter(COD_SS_DATA(:,1),COD_SS_DATA(:,11))
+plot(time,VSS_out)
+
+title("VSS","VSS in effluent (Modelled and Data)")
+xlabel("Time (Days)")
+ylabel("VSS (kg m^-^3)")
+grid on
+axis([127 566 0 1])
+legend("Data","Model")
 
 
